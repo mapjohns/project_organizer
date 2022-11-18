a648ab7b3aeab9d62adeab592db797a76b53c304
Matthew <mattj20@gmail.com>
2022-11-18 11:24:29 -0800

    Added seed data for projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+project_a = Project.create(name:"Make peanut butter and jelly sandwich", description:"Procure the necessary ingredients and craft this sandwich", due_date: DateTime.new(2022, 11, 20, 12, 0, 0), status:"Incomplete")
+project_b = Project.create(name:"Do laundry", description:"It's that time of day, laundry day", due_date: DateTime.new(2022, 11, 22, 12, 0, 0), status:"Incomplete")
