c433ea192cf1565f8e0ea1f5d1d64ef824b16312
Matthew <mattj20@gmail.com>
2022-11-19 17:57:05 -0800

    Added fetch for projects and calls function to add project names to page

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Loaded!")
   });
+
+
+fetch('http://localhost:3000/projects')
+.then(resp => resp.json())
+.then(data => addProjects(data))
+
+function addProjects(projects) {
+    let addProject
+    projects.map(function(a) {
+        addProject = document.createElement('h1'),
+        addProject.innerHTML = a['name']
+        document.querySelector('.projects').append(addProject)
+    }
+        )
+}
