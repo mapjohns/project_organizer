8f70e048619508b38543ac6d75283c84d3a42707
Matthew <mattj20@gmail.com>
2022-11-26 11:58:26 -0800

    Added task_params and updated Create to save task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,13 @@ class TasksController < ApplicationController
 
     def create
         task = Task.new(name: params[:name], project_id: params[:project_id])
+        task.save
         render json: task, except: [:created_at, :updated_at]
+    end
+
+    private
+
+    def task_params
+        params.require(:task).permit(:name, :project_id, :status)
     end
 end
