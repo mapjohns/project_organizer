64cfea99f97c253cd2126649f3cca7177c190560
Matthew <mattj20@gmail.com>
2022-11-26 11:52:54 -0800

    Create action added to handle task creation and render json

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,7 @@
 class TasksController < ApplicationController
+
+    def create
+        task = Task.new(name: params[:name], project_id: params[:project_id])
+        render json: task, except: [:created_at, :updated_at]
+    end
 end
