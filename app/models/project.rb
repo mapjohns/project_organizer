bd27304a9dce0a0a607e409312498debbda58fbc
Matthew <mattj20@gmail.com>
2022-12-03 18:35:00 -0800

    Reverted changes to project model for validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,10 +1,3 @@
 class Project < ApplicationRecord
     has_many :tasks, dependent: :destroy
-    validate :complete_project, on: :update
-
-    def complete_project
-    if self.tasks.select {|task| task.status == "Incomplete"}.count != 0
-        errors.add(:status, "Must complete all tasks")
-    end
-    end    
 end
